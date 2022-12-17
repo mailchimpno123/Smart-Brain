388e7326d4ecb747f730d991104476521c4188f1
Dave Carbonara <carbonara@Daves-MacBook-Pro.local>
2022-12-17 16:35:12 +0100

    update

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -23,7 +23,7 @@ import React, { useState } from 'react';
   })
   .then(response => response.json())
   .then(user => {
-    if(user.id){ // does the user exist? Did we receive a user with a property of id?
+    if(user.id){ 
       props.loadUser(user);
       props.onRouteChange('home');
     }
